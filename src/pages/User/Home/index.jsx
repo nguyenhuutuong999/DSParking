8414ae4ad1f77a2720a0dcb1b2cdd07854e1043e
Ni2401 <Ni2401@users.noreply.github.com>
2020-10-09 19:00:46 +0700

    [Add Evaluation Page]

--- src/pages/User/Home/index.jsx
+++ src/pages/User/Home/index.jsx
@@ -3,28 +3,9 @@ import React from 'react';
 
 function Account() {
     return(
-        <div className="status-line">
-              <div className="flex-box">
-                <div className="name-depart" >Hoa Khanh</div>
-                <div className="value" style={{ color: "rgb(173, 87, 87)" }}>200.000 S</div>
+       <div className="home">
 
-              </div>
-              <div className="flex-box">
-                <div className="name-depart">Nguyen Van Linh</div>
-                <div className="value" style={{ color: "rgb(128, 209, 223)" }}>200.000 S</div>
-
-              </div>
-              <div className="flex-box">
-                <div className="name-depart">Quang Trung</div>
-                <div className="value" style={{ color: "rgb(173, 87, 87)" }}>200.000 S</div>
-
-              </div>
-              <div className="flex-box">
-                <div className="name-depart">Bach Dang</div>
-                <div className="value" style={{ color: "rgb(128, 209, 223)" }}> 200.000 S</div>
-
-              </div>
-            </div>
+       </div> 
     )
 }
 export default Account;
--- src/pages/User/Support/index.jsx
+++ src/pages/User/Support/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , {useState} from 'react';
 import './styles.css';
 
 import { Radio, Form, Input, Button, Select, DatePicker } from 'antd';
@@ -9,28 +9,26 @@ function Support() {
     console.log(`radio checked:${e.target.value}`);
   }
 
-  return (
-    <div className="support">
-      <div className="radio-group">
-        <Radio.Group onChange={onChange} defaultValue="a">
-          <Radio.Button value="a">Hướng dẫn sử dụng</Radio.Button>
-          <Radio.Button value="b">Báo cáo sự cố</Radio.Button>
-        </Radio.Group>
-      </div>
-      <div className="support-main">
-        <div className="support-header">
-          <p><FaBookReader />HƯỚNG DẪN SỬ DỤNG<span>DSParking</span></p>
-        </div>
-        <div className="support-content">
-          <div className="support-content-details">
-            <div className="support-manual">
+  const [isShowManual, setIsShowManual] = useState(false);
+  const [isShowReport, setIsShowReport] = useState(false);
+  const renderSupport = () => {
+    if(isShowManual == true) {
+      return(
+        <>
+          <div className="support-manual">
               <p><span>1. Đăng nhập: </span>Người dùng sử dụng tài khoản myDTU đã có sẵn để đăng nhập vào hệ thống DSParking.</p>
               <p><span>2. Chức năng thay đổi/ lấy mã QR Code:</span> Ở thanh menu, click vào <FaUser /> ‘Thông tin cá nhân’ để lấy mã ( đối với lần sử dụng đầu tiên) / thay đổi mã (đối với những lần sử dụng sau đó).</p>
               <p><span>3. Nạp thẻ / Liên kết tài khoản Ngân hàng:</span> Để thực hiện chức năng này bạn cần click vào  <FaAddressCard /> ‘ Tài khoản DSPay’.</p>
               <p>Nếu bạn chọn phương thức thanh toán bằng cách nạp thẻ: click vào nút Nạp thẻ, sau        đó một textbox sẽ hiện ra, bạn cần điền đầy đủ thoong tin có trong textbox và nhấn OK.</p>
               <p>Nếu bạn chọn phương thức thanh toán bằng cách liên kêt với tài khoản Ngân hàng (đã có sẵn): click vào nút Liên kết, sau đó sẽ được chuyển đến mọt trang mới, bạn cần điền tất cả thông tin được yêu cầu cung cấp. Sau đó nhấn OK và đợi trong vài giây để thực hiện thao tác liên kết.</p>
-            </div>
-            <div className="support-report">
+          </div>
+        </>
+      )
+    }
+    else if (isShowReport == true) {
+      return(
+        <>
+        <div className="support-report">
               <Form
                 labelCol={{ span: 9 }}
                 wrapperCol={{ span: 10 }}
@@ -48,6 +46,25 @@ function Support() {
                 <Button style={{ marginLeft: '550px' }}>Gửi</Button>
               </Form>
             </div>
+        </>
+      )
+    }
+  }
+  return (
+    <div className="support">
+      <div className="radio-group">
+        <Radio.Group onChange={onChange} defaultValue="a">
+          <Radio.Button onClick= {() =>  setIsShowManual(true)} value="a">Hướng dẫn sử dụng</Radio.Button>
+          <Radio.Button onClick= {() =>  setIsShowReport(true)} value="b">Báo cáo sự cố</Radio.Button>
+        </Radio.Group>
+      </div>
+      <div className="support-main">
+        <div className="support-header">
+          <p><FaBookReader />HƯỚNG DẪN SỬ DỤNG<span>DSParking</span></p>
+        </div>
+        <div className="support-content">
+          <div className="support-content-details">
+            {renderSupport()}
           </div>
         </div>
         <div className="footer-support">
