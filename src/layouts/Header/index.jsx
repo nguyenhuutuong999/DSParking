42ab5f5b7179b100841e27ce706c7a7d86f6a729
Ni2401 <Ni2401@users.noreply.github.com>
2020-12-21 12:49:40 +0700

    [Update] header

--- src/layouts/Header/index.jsx
+++ src/layouts/Header/index.jsx
@@ -1,158 +1,179 @@
-import React, { useState, useEffect } from 'react';
-import './styles.css'
+import React, { useState, useEffect } from "react";
+import "./styles.css";
 
-import { Space, Badge, Dropdown, Menu, Button, Modal } from 'antd';
-import { RollbackOutlined, ExclamationCircleOutlined, DollarOutlined } from '@ant-design/icons';
-import { FaBell, FaQrcode, FaBirthdayCake, FaUser, FaExclamationCircle } from 'react-icons/fa';
-
-import history from '../../util/history'
-
-import AvatarDefault from '../../img/avatardefault.jpg'
-
-import QRCode from 'qrcode.react';
-
+import { Space, Badge, Dropdown, Menu, Button, Modal } from "antd";
 import {
-  firebaseApp,
-} from '../../configs/firebase'
-import MenuItem from 'antd/lib/menu/MenuItem';
+  RollbackOutlined,
+  ExclamationCircleOutlined,
+  DollarOutlined,
+} from "@ant-design/icons";
+import {
+  FaBell,
+  FaQrcode,
+  FaBirthdayCake,
+  FaUser,
+  FaExclamationCircle,
+} from "react-icons/fa";
+
+import history from "../../util/history";
+
+import AvatarDefault from "../../img/avatardefault.jpg";
+
+import QRCode from "qrcode.react";
+
+import { firebaseApp } from "../../configs/firebase";
+import MenuItem from "antd/lib/menu/MenuItem";
 
 function Header({ signOut }) {
   const [isShowQrModal, setIsShowQrModal] = useState(false);
   const [userData, setUserData] = useState({});
 
-  const user = JSON.parse(localStorage.getItem('user'));
+  const user = JSON.parse(localStorage.getItem("user"));
 
   useEffect(() => {
-    firebaseApp.database().ref(`/User/information/${user.position === "1"?"parkingMan":"admin"}/${user.id}`).on('value', (snapshot) => {
-      setUserData({ ...snapshot.val() });
-    })
-  }, [])
+    firebaseApp
+      .database()
+      .ref(`/User/information/parkingMan/${user.id}`)
+      .on("value", (snapshot) => {
+        setUserData({ ...snapshot.val() });
+      });
+  }, []);
 
   const handleShowQrModal = () => {
     setIsShowQrModal(true);
-  }
+  };
 
   const handleHideQrModal = () => {
     setIsShowQrModal(false);
-  }
+  };
 
   const handleLogout = () => {
-    
-    localStorage.removeItem('user');
-    return history.push('/login');
-  }
+    firebaseApp.auth().signOut();
+    localStorage.removeItem("user");
+    return history.push("/login");
+  };
 
   const handleChangeQRCode = () => {
-    firebaseApp.database().ref(`/users/${user.id}`).update({
-      qrPin: Math.random().toString().substr(2, 4),
-    })
-  }
+    firebaseApp
+      .database()
+      .ref(`/User/information/parkingMan/${user.id}`)
+      .update({
+        secretNum: Math.random().toString().substr(2, 4),
+      });
+  };
 
   const renderDropdownAvatar = () => {
     return (
       <Menu>
-        <Menu.Item onClick={() => history.push('/profile')} >
+        <Menu.Item onClick={() => history.push("/profile")}>
           <Space>
             <FaUser />
-            <p> Thông tin cá nhân</p>
+            <p>Profile</p>
           </Space>
         </Menu.Item>
         <Menu.Item onClick={() => handleLogout()}>
           <Space>
             <RollbackOutlined />
-            <p>Đăng xuất</p>
+            <p>Log Out</p>
           </Space>
         </Menu.Item>
       </Menu>
-    )
-  }
+    );
+  };
   const renderDropdownNotification = () => {
     return (
-      <Menu className="noti-dropdown-menu" style={{ width: 300, }}>
+      <Menu className="noti-dropdown-menu" style={{ width: 300 }}>
         <div className="noti-dropdown-title">
-          <h6>Bạn có 5 thông báo mới</h6>
+          <h6>You have 5 new notifications</h6>
         </div>
-        <Menu.Item style={{ height: '40px', borderBottom: '0.2px solid #ededed' }}>
+        <Menu.Item
+          style={{ height: "40px", borderBottom: "0.2px solid #ededed" }}
+        >
           <Space>
-            <FaBirthdayCake style={{ fill: '#8dc63f' }} />
-            <p>Chúc mừng sinh nhật Nguyễn T Bích Ni</p>
+            <FaBirthdayCake style={{ fill: "#8dc63f" }} />
+            <p>Happy Birth Day Nguyễn T Bích Ni</p>
           </Space>
         </Menu.Item>
-        <Menu.Item style={{ height: '40px', borderBottom: '0.2px solid #ededed' }}>
+        <Menu.Item
+          style={{ height: "40px", borderBottom: "0.2px solid #ededed" }}
+        >
           <Space>
-            <FaExclamationCircle style={{ fill: '#cc3f44' }} />
-            <p>Số dư trong tài khoản dưới 5000</p>
+            <FaExclamationCircle style={{ fill: "#c44a8a" }} />
+            <p>The balance in the account is less than 5000</p>
           </Space>
         </Menu.Item>
-        <MenuItem style={{ backgroundColor: '#f5f5f5' }} onClick={() => history.push('/notifications')}>
-          <a href="#" style={{ textAlign: 'center', color: '#979898' }}>---Xem tất cả---</a>
+        <MenuItem
+          style={{ backgroundColor: "#f5f5f5" }}
+          onClick={() => history.push("/notifications")}
+        >
+          <a href="#" style={{ textAlign: "center", color: "#979898" }}>
+            ---All---
+          </a>
         </MenuItem>
       </Menu>
-    )
-  }
-  const sidebarr = () => {
-   var local = JSON.parse(localStorage.getItem("user")) ;
-    if(local.position === "2"){
-      return (
-        <div className="app-header">
-        <div className="welcome">
-          <p>Welcome Back, {userData.name} !!!</p>
-          </div>
-      </div>
-        )
-    }else{
-      return(
-        
-        <div className="app-header">
-        <div className="welcome">
-          <p>Welcome Back, {userData.name} !!!</p>
-    
-        </div>
-        <Space className="header-right" align="center" size="middle">
-          <div className="header-balance">
-            <Space>
-              <DollarOutlined />
-              <p>{parseInt(userData.money)?.toLocaleString('it-IT', {style : 'currency', currency : 'VND'})}</p>
-            </Space>
-          </div>
-          <div className="div-svg-header">
-            <FaQrcode onClick={() => handleShowQrModal()} />
-          </div>
-    
-          <div className="div-svg-header">
-    
-            <Dropdown overlay={renderDropdownNotification()} placement="bottomRight">
-              {/* <Badge count={2}> */}
-              <FaBell />
-              {/* </Badge> */}
-            </Dropdown>
-          </div>
-    
-          <Dropdown overlay={renderDropdownAvatar()} placement="bottomCenter">
-            <img style={{ width: '35px', height: '35px', borderRadius: '50%' }} src={userData.avatar ? userData.avatar : AvatarDefault} alt="Avatar" />
-          </Dropdown>
-        </Space>
-    
-        <Modal
-          title="QRCode của bạn: "
-          visible={isShowQrModal}
-          footer={false}
-          onOk={handleHideQrModal}
-          onCancel={handleHideQrModal}
-        >
-          <QRCode value={`${user.id}${userData.secretNum}`} size={250} style={{ margin: '0 110px' }} />
-          <div onClick={() => handleChangeQRCode()}><Button type="primary" ghost >Đổi QRCode</Button></div>
-        </Modal>
-      </div>
-    
-      )
-      
-    }
-   
-  }
+    );
+  };
 
   return (
-    sidebarr()
+    <div className="app-header">
+      <div className="welcome">
+        <p>Hi, {userData.name} !!!</p>
+      </div>
+      <Space className="header-right" align="center" size="middle">
+        <div className="header-balance">
+          <Space>
+            <DollarOutlined />
+            <p>
+              {parseInt(userData.money)?.toLocaleString("it-IT", {
+                style: "currency",
+                currency: "VND",
+              })}
+            </p>
+          </Space>
+        </div>
+        <div className="div-svg-header">
+          <FaQrcode onClick={() => handleShowQrModal()} />
+        </div>
+
+        <div className="div-svg-header">
+          <Dropdown
+            overlay={renderDropdownNotification()}
+            placement="bottomRight"
+          >
+            {/* <Badge count={2}> */}
+            <FaBell />
+            {/* </Badge> */}
+          </Dropdown>
+        </div>
+
+        <Dropdown overlay={renderDropdownAvatar()} placement="bottomCenter">
+          <img
+            style={{ width: "35px", height: "35px", borderRadius: "50%" }}
+            src={userData.avatar ? userData.avatar : AvatarDefault}
+            alt="Avatar"
+          />
+        </Dropdown>
+      </Space>
+
+      <Modal
+        title="Your QRCode: "
+        visible={isShowQrModal}
+        footer={false}
+        onOk={handleHideQrModal}
+        onCancel={handleHideQrModal}
+      >
+        <QRCode
+          value={`${user.id}${userData.secretNum}`}
+          size={250}
+          style={{ margin: "0 110px" }}
+        />
+        <div onClick={() => handleChangeQRCode()} className="change-qrcode">
+          <Button type="primary" ghost>
+            Change QRCode
+          </Button>
+        </div>
+      </Modal>
+    </div>
   );
 }
 
