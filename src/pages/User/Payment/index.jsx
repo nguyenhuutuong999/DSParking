5aa671639c5fc4ef62bed4cb3148af4d53ccf9b1
nguyenhuutuong999 <nguyenhuutuong999@gmail.com>
2021-05-02 13:24:50 +0700

    update Payment func

--- src/pages/User/Payment/index.jsx
+++ src/pages/User/Payment/index.jsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
 import "./styles.css";
 import backgroundP from "../../../img/payment.svg";
 import { Radio } from "antd";
 import axios from "axios";
-import history from "../../../util/history";
 import { firebaseApp } from "../../../configs/firebase";
 
 function Payment() {
@@ -15,58 +13,39 @@ function Payment() {
 
   const [isPopUp, setIsPopUp] = useState(false);
   const [amount, setAmount] = useState(0);
+  const [gateway, setGateWay] = useState('momo');
   const [statusMess, setStatusMess] = useState(false);
   const onChangeValue = (e) => {
     setOrder({ ...order, value: e.target.value });
   };
   useEffect(() => {
+    // get param from URL
     var url_string = window.location.href.toString();
     let url = new URL(url_string);
-    let statusCode = url.searchParams.get("errorCode");
-    let amount = url.searchParams.get("amount");
+
+    //get status code from URL
+    let statusCode = url.searchParams.get("errorCode") ? url.searchParams.get("errorCode") : (url.searchParams.get("vnp_ResponseCode") ? url.searchParams.get("vnp_ResponseCode") : null)
+    // let statusCode = url.searchParams.get("errorCode");
+    // let vnp_ResponseCode = url.searchParams.get("vnp_ResponseCode");
+
+    //get value from URL
+    let amount = url.searchParams.get("amount") ? url.searchParams.get("amount") : (url.searchParams.get("vnp_Amount") ? url.searchParams.get("vnp_Amount") : null);
+    // let amount = url.searchParams.get("amount");
+    // let vnp_Amount = url.searchParams.get("vnp_Amount");
 
     if (statusCode) {
       setIsPopUp(true);
       setAmount(amount);
       if (statusCode == 0) {
-        firebaseApp
-          .database()
-          .ref("User/information/parkingMan/idrootsv1")
-          .once("value", (snapshot) => {
-            //console.log(snapshot.val().money);
-            firebaseApp
-              .database()
-              .ref("User/information/parkingMan/")
-              .child("idrootsv1")
-              .update({
-                money: (
-                  parseInt(snapshot.val().money) + parseInt(amount)
-                ).toString(),
-              });
-          });
-
-          firebaseApp
-          .database()
-          .ref("History/parkingMan/moneyTopUp")
-          .child("idrootsv1")
-          .set({
-            idPay: Date.now().toString(36) + Math.random().toString(36).substr(2),
-            value: parseInt(amount).toString(),
-            createAt: Date.now().toLocaleString(),
-            method: 1,
-            isNoti: false,
-            fee: 0,
-          });
-
+        
         setStatusMess(true);
       }
     }
   }, []);
   const onSubmit = () => {
     axios
-      .post("http://localhost:3001/payment/create", order)
+      .post(`http://localhost:3001/payment/${gateway}`, order)
       .then(function (response) {
-        // http://localhost:3000/?partnerCode=MOMOSDEI20201203&accessKey=ArJY3B0zEJuJlaID&requestId=ae4314c0-a731-11eb-b56f-f1d8d2e6f484&amount=50000&orderId=ae42edb0-a731-11eb-b56f-f1d8d2e6f484&orderInfo=pay%20with%20MoMo&orderType=momo_wallet&transId=2510789992&message=Success&localMessage=Th%C3%A0nh%20c%C3%B4ng&responseTime=2021-04-27%2015:26:36&errorCode=0&payType=qr&extraData=merchantName=;merchantId=&signature=c88bd277af26b20ef34c3d11eae23650738e9d48c72ee09039f6af374df65c15
         window.location.replace(response.data);
       })
       .catch(function (error) {
@@ -74,6 +53,7 @@ function Payment() {
       });
   };
   return (
+
     <div className="container-payment">
       <div className="contents">
         <p className="title-payment">TOP-UP</p>
@@ -86,9 +66,9 @@ function Payment() {
               placeholder="Enter value"
               className="input"
               type="number"
-              id="fname"
-              name="fname"
+              onwheel="this.blur()"
               value={order.value}
+              onChange={(e) => setOrder({ ...order, value: e.target.value })}
             ></input>
           </div>
         </div>
@@ -125,12 +105,12 @@ function Payment() {
             </Radio.Button>
           </Radio.Group>
         </div>
-        <div className="checkout-button">
+        <div className="checkout-button" onChange = {(e) => setGateWay(e.target.value)}>
           <div
             className="wrapper-radio"
             style={{ display: "flex", alignItems: "center" }}
           >
-            <input type="radio" value="momo" name="gateway" />
+            <input type="radio" checked = {gateway === 'momo'} value="momo" name="gateway"  />
             <div
               className="content"
               style={{
@@ -140,6 +120,27 @@ function Payment() {
               }}
             >
               <span className="checkout-title">Payment by Momo Wallet</span>
+              <img
+                src="https://developers.momo.vn/images/logo.png"
+                width="25"
+                style={{ marginLeft: "10px" }}
+              />
+            </div>
+          </div>
+          <div
+            className="wrapper-radio"
+            style={{ display: "flex", alignItems: "center" }}
+          >
+            <input type="radio" checked = {gateway == 'vnpay'} value="vnpay" name="gateway" />
+            <div
+              className="content"
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginLeft: "10px",
+              }}
+            >
+              <span className="checkout-title">Payment by VNPay</span>
               <img
                 src="https://developers.momo.vn/images/logo.png"
                 width="25"
